a38e1b575df112a8bf36493fd5723f8b77e8e53b
Alexandr Doiban <theboxer144@gmail.com>
2025-12-24 20:45:15 +0100

    NotFoundError class refactoring

--- todo_app/src/errors/NotFoundError.ts
+++ todo_app/src/errors/NotFoundError.ts
@@ -1,10 +1,4 @@
 // src/errors/NotFoundError.ts
 export class NotFoundError extends Error {
-  statusCode: number;
-
-  constructor(message: string) {
-    super(message);
-    this.name = 'NotFoundError';
-    this.statusCode = 204;
-  }
+  statusCode = 204;
 }
